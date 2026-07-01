877f5864f193492308d209e8a5a3327b8d4a8291
Adarsh Inaganti <adarshinaganti006@gmail.com>
2026-07-01 14:46:53 +0530

    feat: add multi-tab browsing

--- main.c
+++ main.c
@@ -2,15 +2,26 @@
 #include <string.h>
 #include <webkit/webkit.h>
 
-typedef struct {
+typedef struct AppState AppState;
+typedef struct TabState TabState;
+
+struct AppState {
     GtkWidget *window;
+    GtkWidget *notebook;
     GtkWidget *back_button;
     GtkWidget *forward_button;
     GtkWidget *reload_button;
     GtkWidget *address_entry;
+    GtkWidget *new_tab_button;
+};
+
+struct TabState {
+    AppState *app;
+    GtkWidget *page;
+    GtkWidget *title_label;
     WebKitWebView *web_view;
     gboolean is_loading;
-} AppState;
+};
 
 static char *
 normalize_uri(const char *input)
@@ -34,100 +45,198 @@ normalize_uri(const char *input)
     return g_strdup_printf("https://%s", input);
 }
 
-static void
-update_navigation(AppState *state)
+static TabState *
+get_tab_from_page(GtkWidget *page)
 {
-    gtk_widget_set_sensitive(state->back_button,
-                             webkit_web_view_can_go_back(state->web_view));
-    gtk_widget_set_sensitive(state->forward_button,
-                             webkit_web_view_can_go_forward(state->web_view));
+    if (page == NULL) {
+        return NULL;
+    }
+
+    return g_object_get_data(G_OBJECT(page), "tab-state");
+}
+
+static TabState *
+get_current_tab(AppState *app)
+{
+    GtkWidget *page;
+    int page_num;
+
+    page_num = gtk_notebook_get_current_page(GTK_NOTEBOOK(app->notebook));
+    if (page_num < 0) {
+        return NULL;
+    }
+
+    page = gtk_notebook_get_nth_page(GTK_NOTEBOOK(app->notebook), page_num);
+    return get_tab_from_page(page);
 }
 
 static void
-load_address(AppState *state)
+update_tab_label(TabState *tab)
+{
+    const char *title;
+    const char *uri;
+
+    title = webkit_web_view_get_title(tab->web_view);
+    if (title != NULL && *title != '\0') {
+        gtk_label_set_text(GTK_LABEL(tab->title_label), title);
+        return;
+    }
+
+    uri = webkit_web_view_get_uri(tab->web_view);
+    if (uri != NULL && *uri != '\0') {
+        gtk_label_set_text(GTK_LABEL(tab->title_label), uri);
+        return;
+    }
+
+    gtk_label_set_text(GTK_LABEL(tab->title_label), "New Tab");
+}
+
+static void
+sync_window_to_tab(TabState *tab)
+{
+    AppState *app;
+    const char *title;
+    const char *uri;
+
+    if (tab == NULL) {
+        return;
+    }
+
+    app = tab->app;
+    uri = webkit_web_view_get_uri(tab->web_view);
+    title = webkit_web_view_get_title(tab->web_view);
+
+    gtk_editable_set_text(GTK_EDITABLE(app->address_entry), uri != NULL ? uri : "about:blank");
+    gtk_widget_set_sensitive(app->back_button, webkit_web_view_can_go_back(tab->web_view));
+    gtk_widget_set_sensitive(app->forward_button, webkit_web_view_can_go_forward(tab->web_view));
+    gtk_button_set_icon_name(GTK_BUTTON(app->reload_button),
+                             tab->is_loading
+                                 ? "process-stop-symbolic"
+                                 : "view-refresh-symbolic");
+    gtk_window_set_title(GTK_WINDOW(app->window), title != NULL ? title : "ubar");
+}
+
+static void
+configure_web_view(WebKitWebView *web_view)
+{
+    WebKitSettings *settings;
+
+    settings = webkit_web_view_get_settings(web_view);
+    webkit_settings_set_hardware_acceleration_policy(
+        settings,
+        WEBKIT_HARDWARE_ACCELERATION_POLICY_NEVER);
+    webkit_settings_set_enable_webgl(settings, FALSE);
+    webkit_settings_set_enable_2d_canvas_acceleration(settings, FALSE);
+    webkit_settings_set_enable_write_console_messages_to_stdout(settings, TRUE);
+    webkit_settings_set_enable_developer_extras(settings, TRUE);
+}
+
+static void
+load_address(AppState *app)
 {
     const char *input;
     char *uri;
+    TabState *tab;
 
-    input = gtk_editable_get_text(GTK_EDITABLE(state->address_entry));
+    tab = get_current_tab(app);
+    if (tab == NULL) {
+        return;
+    }
+
+    input = gtk_editable_get_text(GTK_EDITABLE(app->address_entry));
     uri = normalize_uri(input);
     if (uri == NULL) {
         return;
     }
 
-    webkit_web_view_load_uri(state->web_view, uri);
+    webkit_web_view_load_uri(tab->web_view, uri);
     g_free(uri);
 }
 
 static void
 on_back_clicked(GtkButton *button, gpointer user_data)
 {
-    AppState *state;
+    AppState *app;
+    TabState *tab;
 
     (void)button;
-    state = user_data;
-    webkit_web_view_go_back(state->web_view);
+    app = user_data;
+    tab = get_current_tab(app);
+    if (tab != NULL) {
+        webkit_web_view_go_back(tab->web_view);
+    }
 }
 
 static void
 on_forward_clicked(GtkButton *button, gpointer user_data)
 {
-    AppState *state;
+    AppState *app;
+    TabState *tab;
 
     (void)button;
-    state = user_data;
-    webkit_web_view_go_forward(state->web_view);
+    app = user_data;
+    tab = get_current_tab(app);
+    if (tab != NULL) {
+        webkit_web_view_go_forward(tab->web_view);
+    }
 }
 
 static void
 on_reload_clicked(GtkButton *button, gpointer user_data)
 {
-    AppState *state;
+    AppState *app;
+    TabState *tab;
 
     (void)button;
-    state = user_data;
-    if (state->is_loading) {
-        webkit_web_view_stop_loading(state->web_view);
+    app = user_data;
+    tab = get_current_tab(app);
+    if (tab == NULL) {
         return;
     }
 
-    webkit_web_view_reload(state->web_view);
+    if (tab->is_loading) {
+        webkit_web_view_stop_loading(tab->web_view);
+        return;
+    }
+
+    webkit_web_view_reload(tab->web_view);
 }
 
 static void
 on_address_activate(GtkEntry *entry, gpointer user_data)
 {
-    AppState *state;
-
     (void)entry;
-    state = user_data;
-    load_address(state);
+    load_address(user_data);
 }
 
 static void
 on_uri_changed(WebKitWebView *web_view, GParamSpec *pspec, gpointer user_data)
 {
-    AppState *state;
-    const char *uri;
+    TabState *tab;
 
+    (void)web_view;
     (void)pspec;
-    state = user_data;
-    uri = webkit_web_view_get_uri(web_view);
-    if (uri != NULL) {
-        gtk_editable_set_text(GTK_EDITABLE(state->address_entry), uri);
+    tab = user_data;
+
+    update_tab_label(tab);
+    if (tab == get_current_tab(tab->app)) {
+        sync_window_to_tab(tab);
     }
 }
 
 static void
 on_title_changed(WebKitWebView *web_view, GParamSpec *pspec, gpointer user_data)
 {
-    AppState *state;
-    const char *title;
+    TabState *tab;
 
+    (void)web_view;
     (void)pspec;
-    state = user_data;
-    title = webkit_web_view_get_title(web_view);
-    gtk_window_set_title(GTK_WINDOW(state->window), title != NULL ? title : "ubar");
+    tab = user_data;
+
+    update_tab_label(tab);
+    if (tab == get_current_tab(tab->app)) {
+        sync_window_to_tab(tab);
+    }
 }
 
 static void
@@ -135,19 +244,17 @@ on_load_changed(WebKitWebView *web_view,
                 WebKitLoadEvent load_event,
                 gpointer user_data)
 {
-    AppState *state;
+    TabState *tab;
 
     (void)web_view;
-    state = user_data;
-    state->is_loading = load_event == WEBKIT_LOAD_STARTED ||
-                        load_event == WEBKIT_LOAD_REDIRECTED ||
-                        load_event == WEBKIT_LOAD_COMMITTED;
+    tab = user_data;
+    tab->is_loading = load_event == WEBKIT_LOAD_STARTED ||
+                      load_event == WEBKIT_LOAD_REDIRECTED ||
+                      load_event == WEBKIT_LOAD_COMMITTED;
 
-    gtk_button_set_icon_name(GTK_BUTTON(state->reload_button),
-                             state->is_loading
-                                 ? "process-stop-symbolic"
-                                 : "view-refresh-symbolic");
-    update_navigation(state);
+    if (tab == get_current_tab(tab->app)) {
+        sync_window_to_tab(tab);
+    }
 }
 
 static gboolean
@@ -157,13 +264,13 @@ on_load_failed(WebKitWebView *web_view,
                GError *error,
                gpointer user_data)
 {
-    AppState *state;
+    TabState *tab;
     GtkAlertDialog *dialog;
 
     (void)web_view;
     (void)load_event;
     (void)failing_uri;
-    state = user_data;
+    tab = user_data;
 
     if (g_error_matches(error, WEBKIT_NETWORK_ERROR, WEBKIT_NETWORK_ERROR_CANCELLED) ||
         g_error_matches(error,
@@ -174,7 +281,7 @@ on_load_failed(WebKitWebView *web_view,
 
     dialog = gtk_alert_dialog_new("%s", error->message);
     gtk_alert_dialog_set_modal(dialog, TRUE);
-    gtk_alert_dialog_show(dialog, GTK_WINDOW(state->window));
+    gtk_alert_dialog_show(dialog, GTK_WINDOW(tab->app->window));
     g_object_unref(dialog);
 
     return FALSE;
@@ -193,14 +300,117 @@ on_permission_request(WebKitWebView *web_view,
 }
 
 static void
+on_switch_page(GtkNotebook *notebook,
+               GtkWidget *page,
+               guint page_num,
+               gpointer user_data)
+{
+    TabState *tab;
+
+    (void)notebook;
+    (void)page_num;
+    (void)user_data;
+    tab = get_tab_from_page(page);
+    sync_window_to_tab(tab);
+}
+
+static void
+on_close_tab_clicked(GtkButton *button, gpointer user_data)
+{
+    TabState *tab;
+    AppState *app;
+    int page_num;
+
+    (void)button;
+    tab = user_data;
+    app = tab->app;
+
+    page_num = gtk_notebook_page_num(GTK_NOTEBOOK(app->notebook), tab->page);
+    if (page_num < 0) {
+        return;
+    }
+
+    if (gtk_notebook_get_n_pages(GTK_NOTEBOOK(app->notebook)) == 1) {
+        gtk_window_destroy(GTK_WINDOW(app->window));
+        return;
+    }
+
+    gtk_notebook_remove_page(GTK_NOTEBOOK(app->notebook), page_num);
+}
+
+static TabState *
+create_tab(AppState *app, const char *uri)
+{
+    GtkWidget *web_view;
+    GtkWidget *tab_box;
+    GtkWidget *close_button;
+    GtkWidget *title_label;
+    TabState *tab;
+
+    tab = g_new0(TabState, 1);
+    tab->app = app;
+
+    web_view = webkit_web_view_new();
+    tab->page = web_view;
+    tab->web_view = WEBKIT_WEB_VIEW(web_view);
+    configure_web_view(tab->web_view);
+
+    title_label = gtk_label_new("New Tab");
+    gtk_label_set_ellipsize(GTK_LABEL(title_label), PANGO_ELLIPSIZE_END);
+    gtk_label_set_max_width_chars(GTK_LABEL(title_label), 24);
+    tab->title_label = title_label;
+
+    tab_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4);
+    close_button = gtk_button_new_from_icon_name("window-close-symbolic");
+    gtk_button_set_has_frame(GTK_BUTTON(close_button), FALSE);
+    gtk_widget_set_focusable(close_button, FALSE);
+    gtk_box_append(GTK_BOX(tab_box), title_label);
+    gtk_box_append(GTK_BOX(tab_box), close_button);
+
+    gtk_widget_set_hexpand(web_view, TRUE);
+    gtk_widget_set_vexpand(web_view, TRUE);
+
+    g_object_set_data_full(G_OBJECT(web_view), "tab-state", tab, g_free);
+
+    gtk_notebook_append_page(GTK_NOTEBOOK(app->notebook), web_view, tab_box);
+    gtk_notebook_set_tab_reorderable(GTK_NOTEBOOK(app->notebook), web_view, TRUE);
+
+    g_signal_connect(close_button, "clicked", G_CALLBACK(on_close_tab_clicked), tab);
+    g_signal_connect(tab->web_view, "notify::uri", G_CALLBACK(on_uri_changed), tab);
+    g_signal_connect(tab->web_view, "notify::title", G_CALLBACK(on_title_changed), tab);
+    g_signal_connect(tab->web_view, "load-changed", G_CALLBACK(on_load_changed), tab);
+    g_signal_connect(tab->web_view, "load-failed", G_CALLBACK(on_load_failed), tab);
+    g_signal_connect(tab->web_view, "permission-request", G_CALLBACK(on_permission_request), tab);
+
+    update_tab_label(tab);
+    webkit_web_view_load_uri(tab->web_view, uri);
+
+    return tab;
+}
+
+static void
+on_new_tab_clicked(GtkButton *button, gpointer user_data)
+{
+    AppState *app;
+    TabState *tab;
+    int page_num;
+
+    (void)button;
+    app = user_data;
+    tab = create_tab(app, "about:blank");
+    page_num = gtk_notebook_page_num(GTK_NOTEBOOK(app->notebook), tab->page);
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(app->notebook), page_num);
+}
+
+static void
 on_activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *vbox;
     GtkWidget *toolbar;
-    GtkWidget *web_view;
-    WebKitSettings *settings;
     AppState *state;
+    TabState *tab;
+    int page_num;
 
     (void)user_data;
 
@@ -222,22 +432,15 @@ on_activate(GtkApplication *app, gpointer user_data)
     state->forward_button = gtk_button_new_from_icon_name("go-next-symbolic");
     state->reload_button = gtk_button_new_from_icon_name("view-refresh-symbolic");
     state->address_entry = gtk_entry_new();
-    web_view = webkit_web_view_new();
-    state->web_view = WEBKIT_WEB_VIEW(web_view);
-    settings = webkit_web_view_get_settings(state->web_view);
+    state->notebook = gtk_notebook_new();
+    state->new_tab_button = gtk_button_new_from_icon_name("list-add-symbolic");
 
-    webkit_settings_set_hardware_acceleration_policy(
-        settings,
-        WEBKIT_HARDWARE_ACCELERATION_POLICY_NEVER);
-    webkit_settings_set_enable_webgl(settings, FALSE);
-    webkit_settings_set_enable_2d_canvas_acceleration(settings, FALSE);
-    webkit_settings_set_enable_write_console_messages_to_stdout(settings, TRUE);
-    webkit_settings_set_enable_developer_extras(settings, TRUE);
-
-    gtk_editable_set_text(GTK_EDITABLE(state->address_entry), "about:blank");
+    gtk_button_set_has_frame(GTK_BUTTON(state->new_tab_button), FALSE);
     gtk_widget_set_hexpand(state->address_entry, TRUE);
-    gtk_widget_set_hexpand(web_view, TRUE);
-    gtk_widget_set_vexpand(web_view, TRUE);
+    gtk_notebook_set_scrollable(GTK_NOTEBOOK(state->notebook), TRUE);
+    gtk_notebook_set_action_widget(GTK_NOTEBOOK(state->notebook),
+                                   state->new_tab_button,
+                                   GTK_PACK_END);
 
     gtk_box_append(GTK_BOX(toolbar), state->back_button);
     gtk_box_append(GTK_BOX(toolbar), state->forward_button);
@@ -245,22 +448,21 @@ on_activate(GtkApplication *app, gpointer user_data)
     gtk_box_append(GTK_BOX(toolbar), state->address_entry);
 
     gtk_box_append(GTK_BOX(vbox), toolbar);
-    gtk_box_append(GTK_BOX(vbox), web_view);
+    gtk_box_append(GTK_BOX(vbox), state->notebook);
     gtk_window_set_child(GTK_WINDOW(window), vbox);
 
     g_signal_connect(state->back_button, "clicked", G_CALLBACK(on_back_clicked), state);
     g_signal_connect(state->forward_button, "clicked", G_CALLBACK(on_forward_clicked), state);
     g_signal_connect(state->reload_button, "clicked", G_CALLBACK(on_reload_clicked), state);
     g_signal_connect(state->address_entry, "activate", G_CALLBACK(on_address_activate), state);
-    g_signal_connect(state->web_view, "notify::uri", G_CALLBACK(on_uri_changed), state);
-    g_signal_connect(state->web_view, "notify::title", G_CALLBACK(on_title_changed), state);
-    g_signal_connect(state->web_view, "load-changed", G_CALLBACK(on_load_changed), state);
-    g_signal_connect(state->web_view, "load-failed", G_CALLBACK(on_load_failed), state);
-    g_signal_connect(state->web_view, "permission-request", G_CALLBACK(on_permission_request), state);
+    g_signal_connect(state->new_tab_button, "clicked", G_CALLBACK(on_new_tab_clicked), state);
+    g_signal_connect(state->notebook, "switch-page", G_CALLBACK(on_switch_page), state);
     g_signal_connect_swapped(window, "destroy", G_CALLBACK(g_free), state);
 
-    update_navigation(state);
-    webkit_web_view_load_uri(state->web_view, "about:blank");
+    tab = create_tab(state, "about:blank");
+    page_num = gtk_notebook_page_num(GTK_NOTEBOOK(state->notebook), tab->page);
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(state->notebook), page_num);
+    sync_window_to_tab(tab);
 
     gtk_window_present(GTK_WINDOW(window));
 }
